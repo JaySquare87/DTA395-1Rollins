9116836d1916023bfcbee77f925bc810763e7091
JJ Jasser <JJASSER@Rollins.edu>
2024-01-24 12:34:37 -0500

    Week 2 Lab

---
+++ Week 2/Week2Lab.R
@@ -0,0 +1,6 @@
+# Load the tidyverse library
+library(tidyverse)
+
+# Read dataset from the following URL
+# https://github.com/JaySquare87/DTA395-1Rollins/blob/main/Week%202/Advertising.csv
+df <- read.csv("https://raw.githubusercontent.com/JaySquare87/DTA395-1Rollins/main/Week%202/Advertising.csv")
