846a0a7b3a2d0a79bd7cde0082d7540f08777b0e
JJ Jasser <JJASSER@Rollins.edu>
2024-01-30 21:39:36 -0500

    Week 3 Lab

---
+++ Week 3/Week3Lab.R
@@ -0,0 +1,5 @@
+# Load the tidyverse library
+library(tidyverse)
+
+# Load the Default dataset
+df <- read.csv("https://raw.githubusercontent.com/JaySquare87/DTA395-1Rollins/main/Week%203/Default.csv", header=TRUE, sep=",")
